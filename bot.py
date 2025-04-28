06a626671979943ebcc3c6d1b8e8203d4fbb84ac
dragun1234 <1234@ukr.net>
2025-04-28 12:05:53 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ dp.include_router(handlers.router)
 
 async def main():
     logging.info("Бот запущен.")
-     await bot.delete_webhook(drop_pending_updates=True)
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
